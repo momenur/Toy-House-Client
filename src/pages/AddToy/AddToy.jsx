4d1f32c958f4f74423891e881ffeb160550ff1b3
momenur <momenurislam6@gmail.com>
2023-05-20 12:19:25 +0600

    Add a toy page design

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -6,75 +6,73 @@ const AddToy = () => {
             
             <div className="flex-shrink-0 w-full my-8 bg-gray-400 rounded-md shadow-2xl card">
             <h1 className="my-8 text-5xl font-bold text-center text-rose-50 "><span className='border-b-2'>Add a Toy!</span></h1>
-                <div className="card-body">
+                <form className="card-body">
                     <div className='gap-8 md:flex'>
                         <div className="md:w-1/2 form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Picture URL</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Photo URL" className="input input-bordered" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Name" className="input input-bordered" />
                         </div>
                     </div>
                     <div className='gap-8 md:flex'>
                         <div className="md:w-1/2 form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Seller Name</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Seller Name" className="input input-bordered" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Seller Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Seller Email" className="input input-bordered" />
                         </div>
                     </div>
                     <div className='gap-8 md:flex'>
                         <div className="md:w-1/2 form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Available Quantity</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Available Quantity" className="input input-bordered" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Sub-Category</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Sub-Category" className="input input-bordered" />
                         </div>
                     </div>
                     <div className='gap-8 md:flex'>
                         <div className="md:w-1/2 form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text"> Seller Picture</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder=" Seller Picture" className="input input-bordered" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Rating</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="Rating" className="input input-bordered" />
                         </div>
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text">Detail</span>
                         </label>
-                        <textarea placeholder="Bio" className="w-full textarea textarea-bordered textarea-md" ></textarea>
+                        <textarea placeholder="Detail" className="w-full textarea textarea-bordered textarea-md" ></textarea>
                     </div>
-
-
-                    {/* <div className="mt-6 form-control">
-                    <button className="btn btn-primary">Login</button>
-                </div> */}
+                    <div className="mt-6 form-control">
+                    <input className="btn btn-primary" type="submit" value="Add a Toy" />
                 </div>
+                </form>
             </div>
         </div>
     );
